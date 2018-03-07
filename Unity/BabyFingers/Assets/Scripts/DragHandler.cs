affecacafde046c60b4f9a21ecff2ee38b5b3f4a
Jacob Kramer <jpnkramer@gmail.com>
2018-03-06 19:30:38 -0700

    Added startings of 4th event, base drag handling

---
+++ Unity/BabyFingers/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public static GameObject selected;
+    Vector3 startPos;
+    Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        selected = gameObject;
+        startPos = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        selected = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if(transform.parent == startParent)
+        {
+            transform.position = startPos;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity/BabyFingers/Assets/Scripts/Goal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Goal : MonoBehaviour, IDropHandler {
+
+    public GameObject item
+    {
+        get
+        {
+            if(transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(!item)
+        {
+            DragHandler.selected.transform.SetParent(transform);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
